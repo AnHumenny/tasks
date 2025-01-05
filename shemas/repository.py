e58a54f882597585101dfae3a4968ced7eb4f521
Andrei Humenniy <an.gumenniy@gmail.com>
2025-01-05 18:50:09 +0300

    update

--- shemas/repository.py
+++ shemas/repository.py
@@ -7,6 +7,17 @@ from shemas.database import DTask, DUser, DTutor, new_session, DPost
 class Repo:
 
     @classmethod
+    async def select_info(cls, login):
+        async with new_session() as session:
+            q = select(DTask).where(DTask.implementer == login)
+            result = await session.execute(q)
+            answer = result.scalar()
+            if answer is None:
+                return None
+            return answer
+
+
+    @classmethod
     async def search_executor_tasks(cls, search):
         if not search:
             return "Ошибка при поиске"
@@ -29,42 +40,6 @@ class Repo:
             except SQLAlchemyError:
                 await session.rollback()
                 return False
-
-
-    @classmethod
-    async def select_user(cls, login, password):
-        async with new_session() as session:
-            q = select(DUser).where(and_(DUser.login == login, DUser.password == password))
-            result = await session.execute(q)
-            answer = result.scalar()
-            if answer is None:
-                return None
-            return True
-
-
-    @classmethod
-    async def select_info_user(cls, login):
-        async with new_session() as session:
-            q = select(DUser.name, DUser.login, DUser.status).where(DUser.login == login)
-            result = await session.execute(q)
-            answer = result.fetchall()
-            if answer:
-                return {'name': answer[0][0],  'login': answer[0][1], 'status': answer[0][2]}
-            return None
-
-
-    @classmethod
-    async def select_all_users(cls) -> list[DUser ]:
-        try:
-            async with new_session() as session:
-                q = select(DUser).order_by(DUser.id.desc())
-                result = await session.execute(q)
-                answer = result.scalars().all()
-                return answer
-        except SQLAlchemyError as e:
-            logging.error(f"Error occurred while selecting users: {e}")
-            return []
-
 
     @classmethod
     async def select_all_tasks(cls):
@@ -90,7 +65,6 @@ class Repo:
                 print(f"Произошла ошибка при выборке задач: {e}")
                 return None
 
-
     @classmethod
     async def select_tasks_user(cls, login):
         async with new_session() as session:
@@ -102,18 +76,6 @@ class Repo:
             except Exception as e:
                 print(f"Произошла ошибка при выборке задач: {e}")
                 return None
-
-
-    @classmethod
-    async def select_info(cls, login):
-        async with new_session() as session:
-            q = select(DTask).where(DTask.implementer == login)
-            result = await session.execute(q)
-            answer = result.scalar()
-            if answer is None:
-                return None
-            return answer
-
 
     @classmethod
     async def create_task(cls, date_created, date_control, facilitator, implementer, describe, priority, stat_task):
@@ -130,68 +92,6 @@ class Repo:
             session.add(new_task)
             await session.commit()
 
-
-    @classmethod
-    async def create_user(cls, date_created_naive, logins, status, name, position, describe, hashed_password):
-        async with new_session() as session:
-            new_user = DUser(
-                date_created=date_created_naive,
-                login=logins,
-                status=status,
-                name=name,
-                post=position,
-                describe=describe,
-                password=hashed_password
-            )
-            session.add(new_user)
-
-            try:
-                await session.commit()
-                print("Пользователь успешно добавлен!")
-                return new_user  # Возврат нового пользователя или его ID
-            except IntegrityError as e:
-                await session.rollback()  # Откат транзакции в случае ошибки
-                print("Ошибка: Дублирование данных или нарушение уникальности.")
-                # Здесь можно обработать исключение, например, логировать его или вернуть сообщение об ошибке
-                return None  # Или вернуть сообщение об ошибке
-            except SQLAlchemyError as e:
-                await session.rollback()  # Откат транзакции в случае ошибки
-                print(f"Ошибка базы данных: {str(e)}")
-                return None  # Или вернуть сообщение об ошибке
-            except Exception as e:
-                await session.rollback()  # Откат транзакции в случае ошибки
-                print(f"Произошла ошибка: {str(e)}")
-                return None  # Или вернуть сообщение об ошибке
-
-
-    @classmethod
-    async def search_all(cls, search, search_type):
-        if not search:
-            return "Ошибка при поиске "
-        async with new_session() as session:
-            try:
-                if search_type == "name":
-                    q = select(DTask).where(DTask.implementer == search)
-                if search_type == "open" or  search_type == "close" or  search_type == "check":
-                    q = select(DTask).where(and_(DTask.implementer == search, DTask.stat_task == search_type))
-                result = await session.execute(q)
-                answer = result.scalars().all()
-                await session.commit()
-                if not answer:
-                    return None
-                return answer
-            except ValueError:
-                return False
-            except NoResultFound:
-                return False
-            except IntegrityError:
-                await session.rollback()
-                return False
-            except SQLAlchemyError:
-                await session.rollback()
-                return False
-
-
     @classmethod
     async def select_task_id(cls, ssid):
         async with new_session() as session:
@@ -202,7 +102,6 @@ class Repo:
                 return None
             return answer
 
-
     @classmethod
     async def delete_task(cls, ssid):
         async with new_session() as session:
@@ -210,7 +109,6 @@ class Repo:
             await session.execute(q)
             await session.commit()
         return
-
 
     @classmethod
     async def select_task_id_personal(cls, ssid, name):
@@ -242,13 +140,74 @@ class Repo:
 
 
     @classmethod
+    async def select_user(cls, login, password):
+        async with new_session() as session:
+            q = select(DUser).where(and_(DUser.login == login, DUser.password == password))
+            result = await session.execute(q)
+            answer = result.scalar()
+            if answer is None:
+                return None
+            return True
+
+    @classmethod
+    async def select_info_user(cls, login):
+        async with new_session() as session:
+            q = select(DUser.name, DUser.login, DUser.status).where(DUser.login == login)
+            result = await session.execute(q)
+            answer = result.fetchall()
+            if answer:
+                return {'name': answer[0][0],  'login': answer[0][1], 'status': answer[0][2]}
+            return None
+
+    @classmethod
+    async def select_all_users(cls) -> list[DUser ]:
+        try:
+            async with new_session() as session:
+                q = select(DUser).order_by(DUser.id.desc())
+                result = await session.execute(q)
+                answer = result.scalars().all()
+                return answer
+        except SQLAlchemyError as e:
+            logging.error(f"Error occurred while selecting users: {e}")
+            return []
+
+    @classmethod
+    async def create_user(cls, date_created_naive, logins, status, name, position, describe, hashed_password):
+        async with new_session() as session:
+            new_user = DUser(
+                date_created=date_created_naive,
+                login=logins,
+                status=status,
+                name=name,
+                post=position,
+                describe=describe,
+                password=hashed_password
+            )
+            session.add(new_user)
+            try:
+                await session.commit()
+                print("Пользователь успешно добавлен!")
+                return new_user
+            except IntegrityError as e:
+                await session.rollback()
+                print("Ошибка: Дублирование данных или нарушение уникальности.")
+                return None
+            except SQLAlchemyError as e:
+                await session.rollback()
+                print(f"Ошибка базы данных: {str(e)}")
+                return None
+            except Exception as e:
+                await session.rollback()
+                print(f"Произошла ошибка: {str(e)}")
+                return None
+
+    @classmethod
     async def delete_user(cls, ssid):
         async with new_session() as session:
             q = delete(DUser).where(DUser.id == int(ssid))
             await session.execute(q)
             await session.commit()
         return
-
 
     @classmethod
     async def update_user(cls, ssid, name, position, status, describe):
@@ -267,7 +226,6 @@ class Repo:
             await session.commit()
         return
 
-
     @classmethod
     async def select_user_id(cls, ssid):
         async with new_session() as session:
@@ -278,16 +236,46 @@ class Repo:
                 return None
             return answer
 
+    @classmethod
+    async def select_user_all(cls):
+        async with new_session() as session:
+            try:
+                q = select(DUser.id, DUser.name).order_by(DUser.id.desc())
+                result = await session.execute(q)
+                rows = result.fetchall()
+                name_user = [(row[0], row[1]) for row in rows]
+                return name_user
+            except Exception as e:
+                print(f"Ничего не найдено: {e}")
+                return None
+
 
     @classmethod
-    async def select_user_post(cls, ssid):
+    async def search_all(cls, search, search_type):
+        if not search:
+            return "Ошибка при поиске "
         async with new_session() as session:
-            q = select(DUser.post).where(DUser.id == int(ssid))
-            result = await session.execute(q)
-            answer = result.scalar()
-            if answer is None:
-                return None
-            return answer
+            try:
+                if search_type == "name":
+                    q = select(DTask).where(DTask.implementer == search)
+                if search_type == "open" or  search_type == "close" or  search_type == "check":
+                    q = select(DTask).where(and_(DTask.implementer == search, DTask.stat_task == search_type))
+                result = await session.execute(q)
+                answer = result.scalars().all()
+                await session.commit()
+                if not answer:
+                    return None
+                return answer
+            except ValueError:
+                return False
+            except NoResultFound:
+                return False
+            except IntegrityError:
+                await session.rollback()
+                return False
+            except SQLAlchemyError:
+                await session.rollback()
+                return False
 
 
     @classmethod
@@ -305,6 +293,16 @@ class Repo:
 
 
     @classmethod
+    async def select_user_post(cls, ssid):
+        async with new_session() as session:
+            q = select(DUser.post).where(DUser.id == int(ssid))
+            result = await session.execute(q)
+            answer = result.scalar()
+            if answer is None:
+                return None
+            return answer
+
+    @classmethod
     async def select_posts_all(cls):
         async with new_session() as session:
             try:
@@ -315,21 +313,6 @@ class Repo:
             except Exception as e:
                 print(f"Ничего не найдено: {e}")
                 return []
-
-
-    @classmethod
-    async def select_user_all(cls):
-        async with new_session() as session:
-            try:
-                q = select(DUser.id, DUser.name).order_by(DUser.id.desc())
-                result = await session.execute(q)
-                rows = result.fetchall()
-                name_user = [(row[0], row[1]) for row in rows]
-                return name_user
-            except Exception as e:
-                print(f"Ничего не найдено: {e}")
-                return None
-
 
     @classmethod
     async def select_posts(cls):
@@ -343,21 +326,15 @@ class Repo:
                 print(f"Произошла ошибка при выборке задач: {e}")
                 return None
 
-
     @classmethod
-    async def add_position(cls, position):
+    async def delete_post(cls, ssid):
+        print("удаляемый id", ssid)
         async with new_session() as session:
-            try:
-                new_post = DPost(position=position)
-                session.add(new_post)
-                await session.commit()
-                return "OK"
-            except IntegrityError:
-                await session.rollback()
-                return 'Должность должна быть уникальной!'
-            except Exception as e:
-                await session.rollback()
-                return f'Произошла ошибка: {str(e)}'
+            q = delete(DPost).where(DPost.id == int(ssid))
+            await session.execute(q)
+            await session.commit()
+        return
+
 
     @classmethod
     async def select_facilitator(cls):
@@ -386,7 +363,6 @@ class Repo:
                 await session.rollback()
                 return f'Произошла ошибка: {str(e)}'
 
-
     @classmethod
     async def delete_facilitator(cls, ssid):
         async with new_session() as session:
@@ -397,10 +373,18 @@ class Repo:
 
 
     @classmethod
-    async def delete_post(cls, ssid):
-        print("удаляемый id", ssid)
+    async def add_position(cls, position):
         async with new_session() as session:
-            q = delete(DPost).where(DPost.id == int(ssid))
-            await session.execute(q)
-            await session.commit()
-        return
+            try:
+                new_post = DPost(position=position)
+                session.add(new_post)
+                await session.commit()
+                return "OK"
+            except IntegrityError:
+                await session.rollback()
+                return 'Должность должна быть уникальной!'
+            except Exception as e:
+                await session.rollback()
+                return f'Произошла ошибка: {str(e)}'
+
+
