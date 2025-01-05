da553ae48bf3b2e7d89070c8daedde3fdf35e879
Andrei Humenniy <an.gumenniy@gmail.com>
2025-01-05 18:16:59 +0300

    update path

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,11 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # абсолютный путь всегда в самом верху до остальных импортов
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, QueuePool
 from alembic import context
+from shemas.database import DTutor
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,13 +13,16 @@ config = context.config
 
 fileConfig(config.config_file_name)
 
-config.set_main_option('sqlalchemy.url', f"{os.getenv('DB_ENGINE')}")
+config.set_main_option('sqlalchemy.url',
+                       f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}'
+                       f':{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}')
 
+target_metadata = DTutor.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
-   # context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -33,7 +38,7 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-     #       target_metadata=target_metadata,
+            target_metadata=target_metadata,
             compare_type=True,
         )
 
